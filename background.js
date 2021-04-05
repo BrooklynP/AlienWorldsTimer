d0e74426ad51a09a1af30c3ae314425e5852396e
BrooklynP <Brooklynpedley@gmail.com>
2021-04-05 19:41:12 +0100

    Core app working

--- background.js
+++ background.js
@@ -1,8 +1,27 @@
-setTimeout(() => {
-    let waxAddress = localStorage.getItem('waxAddress');
-    // alert(waxAddress);
-    console.log(waxAddress);
-    fetch('https://awstats.io/mining/miner/24/' + waxAddress)
-        .then(response => response.text())
-        .then(data => { console.log(data); }).catch(error=>{console.error(error)})
-}, 10000);
+function checkIfMineReady(timeToCheck) {
+    setTimeout(() => {
+        console.log("Checking if mine ready");
+        let waxAddress = localStorage.getItem('waxAddress');
+        fetch('https://awstats.io/mining/miner/24/' + waxAddress)
+            .then(response => response.text())
+            .then(data => {
+                let newData = data.split('<table border="0" class="dataframe w3-table w3-hoverable w3-light-grey top-miner-table" id="all-mines-table">')[1].split('<tbody>')[1].split('<tr>')[1].split('<td>');
+                let mineDate = newData[3].replace('</td>', '').replace(' ', 'T').trim() + 'Z';
+                mineDate = new Date(mineDate);
+                let mineCooldown = parseInt(newData[6].replace('</td>', '').trim());
+                let now = new Date();
+                let mineReset = new Date(mineDate.getTime() + (mineCooldown * 1000))
+                if (now > mineReset) {
+                    console.log("Mine ready");
+                    alert("You're ready to mine");
+                    checkIfMineReady(10000)
+                }
+                else{
+                    console.log("Mine not ready, ready at ", mineReset);
+                    console.log("Checking again in ", mineReset - now)
+                    checkIfMineReady(mineReset - now);
+                }
+            }).catch(error => { console.error(error) })
+    }, timeToCheck);
+}
+checkIfMineReady(10000);
--- index.js
+++ index.js
@@ -6,12 +6,6 @@ function getCurrentlySetWaxAddress() {
     if (waxAddress) {
         document.getElementById('waxAddressDisplay').innerText = 'Your WAX address is set to ' + waxAddress
     }
-    // chrome.storage.local.get(['waxAddress'], function (result) {
-    //     console.log('Value currently is ' + result.key);
-    //     if (result.key) {
-    //         document.getElementById('waxAddressDisplay').innerText = 'Your WAX address is set to ' + result.key
-    //     }
-    // });
 }
 
 function setCurrentWaxAddress() {
@@ -19,9 +13,4 @@ function setCurrentWaxAddress() {
     console.log(waxAddress);
     localStorage.setItem('waxAddress', waxAddress);
     getCurrentlySetWaxAddress();
-    // chrome.storage.local.set({ 'waxAddress': waxAddress }, function (result) {
-    //     console.log(result);
-    //     console.log('Value currently is ' + result.key);
-    //     getCurrentlySetWaxAddress();
-    // });
 }
